88c0cff8fcdbdc67b594222dd312cffd9580e8dc
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-11-30 20:09:33 -0300

    fix: remove fix parameter from bcryptadapter

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import { IEncrypter } from '../../data/protocols/encrypter'
 
 export class BcryptAdapter implements IEncrypter {
-  constructor (private readonly salt: number = 12) {
+  constructor (private readonly salt: number) {
   }
 
   async encrypt (value: string): Promise<string> {
