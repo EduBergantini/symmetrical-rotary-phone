b0d2929d377c0adac283b69e84e630f31d1eda25
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-11-30 08:23:17 -0300

    refactor: change error test from Stub to a Mock

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -6,6 +6,7 @@ interface ISutTypes {
   sut: SignUpController
   emailValidatorStub: IEmailValidator
 }
+
 const makeEmailValidator = (): IEmailValidator => {
   class EmailValidatorStub implements IEmailValidator {
     isValid (email: string): boolean {
@@ -14,14 +15,7 @@ const makeEmailValidator = (): IEmailValidator => {
   }
   return new EmailValidatorStub()
 }
-const makeEmailValidatorWithError = (): IEmailValidator => {
-  class EmailValidatorStub implements IEmailValidator {
-    isValid (email: string): boolean {
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
+
 const makeSut = (): ISutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
@@ -120,8 +114,8 @@ describe('Sign up Controller', () => {
   })
 
   test('should return 500 if EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
     const httpRequest = {
       body: {
         name: 'any_name',
