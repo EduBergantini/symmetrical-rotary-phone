722da0e50094fac7a15cb37e635ce7fb9d4f91cd
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-11-30 08:32:00 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -16,11 +16,12 @@ export class SignUpController implements IController {
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
