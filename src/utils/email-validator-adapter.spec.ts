dcdba6a63d6a4f23601d3c7654440a1c200f63dc
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-11-30 13:40:41 -0300

    refactor: move sut to a factory method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email Validator Adapter', () => {
   test('should return false if validator fails', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_mail@mail.com')
     expect(isValid).toBeFalsy()
   })
 
   test('should return true if validation succeed', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_mail@mail.com')
     expect(isValid).toBeTruthy()
   })
 
   test('should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     const email = 'valid_mail@mail.com'
     sut.isValid(email)
