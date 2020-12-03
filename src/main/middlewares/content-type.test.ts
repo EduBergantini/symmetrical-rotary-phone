4d7fffe895dc01d9f6cd4863f1e1a8c44324d548
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-12-03 08:10:08 -0300

    test: ensure content type returns XML when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -11,4 +11,15 @@ describe('Content-Type Middleware', () => {
       .get(routeName)
       .expect('content-type', /json/)
   })
+
+  test('should return XML content type when forced', async () => {
+    const routeName = '/test_xml_content_type'
+    app.get(routeName, (request, response) => {
+      response.type('xml')
+      response.send('')
+    })
+    await request(app)
+      .get(routeName)
+      .expect('content-type', /xml/)
+  })
 })
