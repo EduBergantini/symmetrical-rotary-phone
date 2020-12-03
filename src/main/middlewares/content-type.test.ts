9febee866c32ac42f12d495e0cb2f1f6bac70e00
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-12-03 08:09:15 -0300

    test: ensure content type returns JSON as default

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content-Type Middleware', () => {
+  test('should return default content type as JSON', async () => {
+    const routeName = '/test_json_content_type'
+    app.get(routeName, (request, response) => {
+      response.send('')
+    })
+    await request(app)
+      .get(routeName)
+      .expect('content-type', /json/)
+  })
+})
