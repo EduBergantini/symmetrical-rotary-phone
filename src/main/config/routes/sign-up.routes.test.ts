8f3103116ebd7c67beb81ed91f12751ac8be1a72
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-12-03 20:27:13 -0300

    refactor: connect to MongoDB before sign-up route integration test

--- src/main/config/routes/sign-up.routes.test.ts
+++ src/main/config/routes/sign-up.routes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest'
+import { MongoHelper } from '../../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('should return an account on success', async () => {
     const routeName = '/api/sign-up'
 
