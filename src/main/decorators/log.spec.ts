09b5f14cb4295d42231006918ce437d220f541b9
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-12-04 21:01:36 -0300

    refactor: move sut to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,26 +1,45 @@
 import { IController, IHttpRequest, IHttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+interface SutTypes {
+  sut: IController
+  controllerStub: IController
+}
+
+const makeControllerStub = (): IController => {
+  class ControllerStub implements IController {
+    async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
+      const httpResponse: IHttpResponse = {
+        statusCode: 200,
+        body: {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@email.com',
+          password: '123@Mudar',
+          passwordConfirmation: '123@Mudar'
+        }
+      }
+      return Promise.resolve(httpResponse)
+    }
+  }
+  const controllerStub = new ControllerStub()
+  return controllerStub
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('Log Controller Decorator', () => {
   test('should call controller handle method', async () => {
-    class ControllerStub implements IController {
-      async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        const httpResponse: IHttpResponse = {
-          statusCode: 200,
-          body: {
-            id: 'any_id',
-            name: 'any_name',
-            email: 'any_email@email.com',
-            password: '123@Mudar',
-            passwordConfirmation: '123@Mudar'
-          }
-        }
-        return Promise.resolve(httpResponse)
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
+
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         name: 'any_name',
