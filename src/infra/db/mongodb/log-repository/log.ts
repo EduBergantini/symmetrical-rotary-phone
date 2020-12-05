6aa9bc792956e6b7719953a408cfc21bddccd707
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-12-05 14:28:31 -0300

    feat(LogErrorMongoRepository): ensure error log is created on success

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,12 @@
+import { ILogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogErrorMongoRepository implements ILogErrorRepository {
+  async logError (errorStack: string): Promise<void> {
+    const collection = await MongoHelper.getCollection('errors')
+    await collection.insertOne({
+      stack: errorStack,
+      date: new Date()
+    })
+  }
+}
