b28a5a21afa5269ee5b81f131f388125137bec17
edubergantini <eduardo_bergantini_pinto@hotmail.com>
2020-12-05 12:08:44 -0300

    refactor: log.spec: move http request and response to a factory

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -4,21 +4,39 @@ import { LogControllerDecorator } from './log'
 interface SutTypes {
   sut: IController
   controllerStub: IController
+  mockedResponse: IHttpResponse
+  mockedRequest: IHttpRequest
+}
+
+const makeMockHttpRequest = (): IHttpRequest => {
+  const httpRequest = {
+    body: {
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: '123@Mudar',
+      passwordConfirmation: '123@Mudar'
+    }
+  }
+  return httpRequest
+}
+
+const makeMockHttpResponse = (httpRequest: IHttpRequest): IHttpResponse => {
+  const httpResponse: IHttpResponse = {
+    statusCode: 200,
+    body: {
+      id: 'any_id',
+      name: httpRequest.body.name,
+      email: httpRequest.body.email,
+      password: httpRequest.body.password
+    }
+  }
+  return httpResponse
 }
 
 const makeControllerStub = (): IController => {
   class ControllerStub implements IController {
     async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-      const httpResponse: IHttpResponse = {
-        statusCode: 200,
-        body: {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email@email.com',
-          password: '123@Mudar',
-          passwordConfirmation: '123@Mudar'
-        }
-      }
+      const httpResponse: IHttpResponse = makeMockHttpResponse(httpRequest)
       return Promise.resolve(httpResponse)
     }
   }
@@ -29,25 +47,22 @@ const makeControllerStub = (): IController => {
 const makeSut = (): SutTypes => {
   const controllerStub = makeControllerStub()
   const sut = new LogControllerDecorator(controllerStub)
+  const mockedRequest = makeMockHttpRequest()
   return {
     sut,
-    controllerStub
+    controllerStub,
+    mockedRequest: makeMockHttpRequest(),
+    mockedResponse: makeMockHttpResponse(mockedRequest)
+
   }
 }
 
 describe('Log Controller Decorator', () => {
   test('should call controller handle method', async () => {
-    const { sut, controllerStub } = makeSut()
+    const { sut, controllerStub, mockedRequest } = makeSut()
 
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@email.com',
-        password: '123@Mudar',
-        passwordConfirmation: '123@Mudar'
-      }
-    }
+    const httpRequest = mockedRequest
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
