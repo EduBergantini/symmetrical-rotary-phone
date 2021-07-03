2220cd777c071a695e8c32feb6c9bd54fb229df3
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-02 22:44:18 -0300

    refactor(ValidationCompositeSpecs): move sut to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,14 +1,30 @@
 import { IValidation } from './validation'
 import { ValidationComposite } from './validation-composite'
+
+interface ISutTypes {
+  sut: ValidationComposite
+  validationStub: IValidation
+}
+
+const makeSut = (): ISutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return { sut, validationStub }
+}
+
+const makeValidation = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
 describe('Required Field Validation', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements IValidation {
-      validate (input: any): Error {
-        return new Error()
-      }
-    }
-
-    const sut = new ValidationComposite([new ValidationStub()])
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
