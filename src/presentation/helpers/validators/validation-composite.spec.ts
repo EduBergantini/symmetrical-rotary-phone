6e4a42d9063c81db55085ac8f634a485b9111973
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-02 22:51:09 -0300

    refactor(ValidationCompositeStub): add 2 validations to the composite

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -3,13 +3,13 @@ import { ValidationComposite } from './validation-composite'
 
 interface ISutTypes {
   sut: ValidationComposite
-  validationStub: IValidation
+  validationStubs: IValidation[]
 }
 
 const makeSut = (): ISutTypes => {
-  const validationStub = makeValidation()
-  const sut = new ValidationComposite([validationStub])
-  return { sut, validationStub }
+  const validationStubs = [makeValidation(), makeValidation()]
+  const sut = new ValidationComposite(validationStubs)
+  return { sut, validationStubs }
 }
 
 const makeValidation = (): IValidation => {
@@ -23,8 +23,8 @@ const makeValidation = (): IValidation => {
 
 describe('Required Field Validation', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new Error())
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
