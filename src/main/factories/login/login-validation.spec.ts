eebf96c1aca3783ab828dd2c96291afee4c6797d
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-02 23:28:37 -0300

    test: create Login Validation Factory

---
+++ src/main/factories/login/login-validation.spec.ts
@@ -0,0 +1,31 @@
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { makeLoginValidation } from './login-validation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { EmailFieldValidation } from '../../../presentation/helpers/validators/email-field-validation'
+import { IValidation } from '../../../presentation/helpers/validators/validation'
+import { IEmailValidator } from '../../../presentation/protocols/email-validator'
+
+jest.mock('../../../presentation/helpers/validators/validation-composite')
+
+describe('Signup Validation Factory', () => {
+  const makeEmailValidator = (): IEmailValidator => {
+    class EmailValidatorStub implements IEmailValidator {
+      isValid (email: string): boolean {
+        return true
+      }
+    }
+    return new EmailValidatorStub()
+  }
+
+  test('Should call validation composite with all IValidation', () => {
+    makeLoginValidation()
+
+    const validationList: IValidation[] = []
+
+    for (const fieldName of ['email', 'password']) {
+      validationList.push(new RequiredFieldValidation(fieldName))
+    }
+    validationList.push(new EmailFieldValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validationList)
+  })
+})
