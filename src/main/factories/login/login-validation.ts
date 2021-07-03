f28d1bea4cafe2657db963932826c64fc06d737d
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-02 23:28:21 -0300

    feat: create Login Validation Factory

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,17 @@
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { IValidation } from '../../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailFieldValidation } from '../../../presentation/helpers/validators/email-field-validation'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeLoginValidation = (): IValidation => {
+  const validationList: IValidation[] = []
+
+  for (const fieldName of ['email', 'password']) {
+    validationList.push(new RequiredFieldValidation(fieldName))
+  }
+  validationList.push(new EmailFieldValidation('email', new EmailValidatorAdapter()))
+  const validationComposite = new ValidationComposite(validationList)
+
+  return validationComposite
+}
