172e5b8b58bc7a96de629a36167c543151d99eda
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-03 13:46:05 -0300

    feat(DbAuthentication): ensure it call IUpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,12 +2,14 @@ import { AuthenticationModel, IAuthentication } from '../../../domain/usecases/a
 import { IHashComparer } from '../../protocols/criptography/hash-comparer'
 import { ITokenGenerator } from '../../protocols/criptography/token-generator'
 import { ILoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { IUpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements IAuthentication {
   constructor (
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
     private readonly hashComparer: IHashComparer,
-    private readonly tokenGenerator: ITokenGenerator
+    private readonly tokenGenerator: ITokenGenerator,
+    private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository
   ) {}
 
   async auth (model: AuthenticationModel): Promise<string> {
@@ -20,6 +22,7 @@ export class DbAuthentication implements IAuthentication {
       return null
     }
     const accessToken = await this.tokenGenerator.generate(accountModel.id)
+    await this.updateAccessTokenRepository.update(accountModel.id, accessToken)
     return accessToken
   }
 }
