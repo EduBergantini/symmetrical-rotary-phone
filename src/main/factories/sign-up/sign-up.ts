8f4f80a9cfe14b97645fe00323495300b57fa8a8
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-03 20:01:23 -0300

    refactor(BCryptAdapter): move files to a specific folder

--- src/main/factories/sign-up/sign-up.ts
+++ src/main/factories/sign-up/sign-up.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { MongoAccountRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogErrorMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../../presentation/controllers/sign-up/sign-up'
