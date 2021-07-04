4a308c3ddbd635f89a127ef3ef0ec1ae5f0d95df
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-04 01:06:50 -0300

    refactor(main): move routes folder out of config folder

--- src/main/config/routes/sign-up.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { adaptRoute } from '../../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../../factories/sign-up/sign-up-factory'
-
-export default (router: Router): void => {
-  router.post('/sign-up', adaptRoute(makeSignUpController()))
-}
