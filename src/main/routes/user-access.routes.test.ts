fc734c1c5a8a76b9b141d1861e07180744a70221
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-04 02:00:52 -0300

    test(routes): ensure login route returns 200 for correct values

--- src/main/routes/user-access.routes.test.ts
+++ src/main/routes/user-access.routes.test.ts
@@ -1,6 +1,11 @@
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+
+let accountCollection: Collection<any>
 
 describe('User Access Routes', () => {
   beforeAll(async () => {
@@ -10,21 +15,40 @@ describe('User Access Routes', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
-  describe('Sign Up Routes (POST /sign-up)', () => {
+  describe('Sign Up Route (POST /sign-up)', () => {
     test('should return 200 on sign up success', async () => {
-      const routeName = '/api/sign-up'
-
       await request(app)
-        .post(routeName)
+        .post('/api/sign-up')
         .send({
           name: 'Meu Nome Completo',
           email: 'meu-email@minha-empresa.com',
           password: '123@Mudar!',
           passwordConfirmation: '123@Mudar!'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('Sign In Route (POST /sign-in)', () => {
+    test('should return 200 on sign in success', async () => {
+      const password = '123@Mudar!'
+      const hashedPassword = await hash(password, 12)
+
+      await accountCollection.insertOne({
+        name: 'Meu Nome Completo',
+        email: 'meu-email@minha-empresa.com',
+        password: hashedPassword
+      })
+
+      await request(app)
+        .post('/api/sign-in')
+        .send({
+          email: 'meu-email@minha-empresa.com',
+          password
         })
         .expect(200)
     })
