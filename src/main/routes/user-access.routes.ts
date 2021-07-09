5c461d0214644a95dab0f8165bd54b795fd53208
EduBergantini <eduardo_bergantini_pinto@hotmail.com>
2021-07-09 15:43:33 -0300

    refactor: rename controller factory files

--- src/main/routes/user-access.routes.ts
+++ src/main/routes/user-access.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/sign-up/sign-up-factory'
-import { makeSignInController } from '../factories/sign-in/sign-in-factory'
+import { makeSignUpController } from '../factories/sign-up/sign-up-controller-factory'
+import { makeSignInController } from '../factories/sign-in/sign-in-controller-factory'
 
 export default (router: Router): void => {
   router.post('/sign-up', adaptRoute(makeSignUpController()))
